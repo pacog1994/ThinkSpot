6c3d1053ee594ccfb0132fa4c34c0ad49ba57f01
Paco Gallegos <pacog1994@gmail.com>
2018-04-30 21:05:29 -0400

    Cutting excess files and testing out react

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function tick() {
+    const element = 
+    <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+ReactDOM.render(element, document.getElementById('root'));
+};
+
+//example of component function with a pre-defined props object 
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+// const welcome = <Welcome name="Sara"/>
+// ReactDOM.render(
+//     welcome, 
+//     document.getElementById('root')
+// );
+
+//example of nested component can use same component abstraction
+function App() {
+    return (
+        <div>
+            <Welcome name="Sara"/>
+            <Welcome name="Cahal"/>
+            <Welcome name="Edite" />
+        </div>
+    );
+}
+
+setInterval(tick, 1000);
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
